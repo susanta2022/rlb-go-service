e18864436fe7275b41f0ac0bfe6e87b6b77a9522
susanta2022 <susanta@adansa.com>
2023-12-17 15:37:28 +0530

    change version 0.1.2

--- services/mysql_service.go
+++ services/mysql_service.go
@@ -3,5 +3,5 @@ package services
 import "fmt"
 
 func PrintConnectionStatus() {
-	fmt.Println("mysql 5.5.62 connection successfull from tag version v0.1.1")
+	fmt.Println("mysql 5.5.62 connection successfull from tag version v0.1.2")
 }
