41dc2a7c10fff40e061e85a639308d9c5e237792
susanta2022 <susanta@adansa.com>
2023-12-17 15:05:46 +0530

    version release 0.1.0

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/susanta2022/rlb-go-service
+
+go 1.21.5
---
+++ services/mysql_service.go
@@ -0,0 +1,7 @@
+package services
+
+import "fmt"
+
+func PrintConnectionStatus() {
+	fmt.Println("mysql 5.5.62 connection successfull")
+}
